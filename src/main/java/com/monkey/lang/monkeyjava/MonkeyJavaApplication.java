176d66610d2bf64924303f77c909e39f221d8ad9
Leo <leogutierrezramirez@gmail.com>
2018-09-08 13:56:55 -0600

    Adding initial files.

---
+++ src/main/java/com/monkey/lang/monkeyjava/MonkeyJavaApplication.java
@@ -0,0 +1,12 @@
+package com.monkey.lang.monkeyjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonkeyJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonkeyJavaApplication.class, args);
+	}
+}
